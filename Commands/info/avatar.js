0fa6a47d708bd79fee33e38b80b1c8d0a160fdf7
unknown_bom <73638811+AahhsSjsj@users.noreply.github.com>
2021-09-15 11:44:51 -0500

    Discord Bot V13

--- Commands/info/avatar.js
+++ Commands/info/avatar.js
@@ -1,21 +1,70 @@
-const { MessageEmbed } = require('discord.js')
+const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
 
 module.exports = {
-    name: 'avatar',
-    aliases: ['pfp'],
-    description: 'Muestra El Avatar Del Usuario',
-    cooldown: 5,
-    execute(message, args, commandName, client, Discord) {
-        const Target = message.mentions.users.first() || message.author;
+  name: "avatar",
+  aliases: ["pfp"],
+  description: "Muestra El Avatar Del Usuario",
+  cooldown: 5,
+  /**
+   *
+   * @param {import("../../index")} client
+   * @param {import("discord.js").Message} message
+   * @param {string[]} args
+   * @param {*} param3
+   */
+  execute(message, args, commandName, client, Discord) {
+    const Target = message.mentions.users.first() || message.author;
 
-        const responder = new MessageEmbed()
-        .setColor('RANDOM')
-        .setAuthor(`${Target.tag}\'s Avatar`)
-        .setTitle('Link As')
-        .setDescription(`[png](${Target.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 })}) | [jpg](${Target.displayAvatarURL({ format: 'jpg', dynamic: true, size: 1024 })}) | [webp](${Target.displayAvatarURL({ format: 'webp', dynamic: true, size: 1024 })}) `)
-        .setImage(Target.displayAvatarURL({ dynamic: true, size: 1024 }))
-        .setFooter(`Requerido Por ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true}))
+    const responder = new MessageEmbed().setColor("RANDOM");
 
-        message.reply({embeds: [responder]})
-    }
-}
+    const image = Target.displayAvatarURL({ dynamic: true, size: 1024 });
+  
+    
+    const png = Target.displayAvatarURL({
+      dynamic: true,
+      format: "png",
+      size: 1024,
+    });
+
+    const jpg = Target.displayAvatarURL({
+      dynamic: true,
+      format: "jpg",
+      size: 1024,
+    });
+
+    const webp = Target.displayAvatarURL({
+      dynamic: true,
+      format: "webp",
+      size: 1024,
+    });
+
+    const jpeg = Target.displayAvatarURL({
+      dynamic: true,
+      format: "jpeg",
+      size: 1024,
+    });
+
+    const gif = Target.displayAvatarURL({
+      dynamic: true,
+      format: "gif",
+      size: 1024,
+    });
+
+    responder.setAuthor(`Avatar De ${Target.username}'s`);
+    responder.setTitle("Links As").setImage(image);
+    responder.setDescription(
+      `[png](${png}) | [jpg](${jpg}) | [webp](${webp}) | [jpeg](${jpeg}) | [gif](${gif}) `
+    );
+
+    const row = new MessageActionRow().addComponents(
+      new MessageButton().setStyle("LINK").setLabel("PNG").setURL(`${png}`),
+      new MessageButton().setStyle("LINK").setLabel("JPG").setURL(`${jpg}`),
+      new MessageButton().setStyle("LINK").setLabel("WEBP").setURL(`${webp}`),
+      new MessageButton().setStyle("LINK").setLabel("JPEG").setURL(`${jpeg}`),
+      new MessageButton().setStyle("LINK").setLabel("GIF").setURL(`${gif}`)
+    );
+
+
+    message.reply({ embeds: [responder], components: [row] });
+    },
+};
