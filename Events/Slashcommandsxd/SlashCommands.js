f6ca7e15e37e4c645b68e46db23a43f34138d2ee
unknown_bom <73638811+AahhsSjsj@users.noreply.github.com>
2021-09-17 06:04:19 -0500

    Discord Bot V13

--- Events/Slashcommandsxd/SlashCommands.js
+++ Events/Slashcommandsxd/SlashCommands.js
@@ -4,7 +4,7 @@ client.on('interactionCreate', async (interaction) => {
     if (interaction.isCommand()) {
         let cmd = client.slash_commands.get(interaction.commandName);
         if (!cmd) return interaction.followUp({
-            content: 'este comando Slash no existe Consulte al server de soporte si sale de nuevo este mensaje'
+            content: 'este comando Slash no existe Consulte al server de soporte si sale de nuevo este mensaje!'
         }) && client.slash_commands.delete(interaction.commandName)
 
         await interaction.deferReply().catch(e => { });
