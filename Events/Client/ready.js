35d3864bae4602026d3e4dfe6e2dfd6064a2d3a1
unknown_bom <73638811+AahhsSjsj@users.noreply.github.com>
2021-09-26 22:45:41 -0500

    Discord Bot V13

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -5,6 +5,6 @@ module.exports = {
         client.user.setActivity('Tu Texto PD: Mensaje Para Poner en el Estado!', {type: 'PLAYING' }) // PLAYING, WATCHING, LISTENING, STREAMING Nota: Para El STREAMING Debes Poner Asi:
         //type: "STREAMING",
         //url: "twitch canal!"
-        //osea client.user.setActivity("Test", {type: 'STREAMING', url: 'https://www.twitch.tv/rubius' })
+        //osea client.user.setActivity("Test", {type: 'STREAMING', url: 'https://www.twitch.tv/rubius' }) :)
     }
 }
