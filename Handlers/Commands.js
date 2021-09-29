8308a07ef5fdeeff725b148e2c546a107335b357
unknown_bom <73638811+AahhsSjsj@users.noreply.github.com>
2021-09-28 23:08:41 -0500

    Discord Bot V13

--- Handlers/Commands.js
+++ Handlers/Commands.js
@@ -1,12 +1,12 @@
-const { readdirSync } = require('fs')
+const { readdirSync } = require('fs');
 
 module.exports = (client, Discord) => {
-    const commandFolders =  readdirSync('Commands')
+    const commandFolders =  readdirSync('Commands');
     for (const folder of commandFolders) {
         const commandFiles = readdirSync(`./Commands/${folder}`).filter(files => files.endsWith('.js'));
         for (const file of commandFiles) {
-            const command = require(`../Commands/${folder}/${file}`)
-            client.commands.set(command.name, command)
+            const command = require(`../Commands/${folder}/${file}`);
+            client.commands.set(command.name, command);
         }
     }
 }
