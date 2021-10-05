70febcfbcf17ec39f02d9cfb1aca1323af9c99a2
unknown_bom <73638811+AahhsSjsj@users.noreply.github.com>
2021-10-05 01:37:54 -0500

    Discord Bot V13

--- slashCommands/util/ping.js
+++ slashCommands/util/ping.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: "ping",
-  description: "mira el ping",
+  description: "mira el ping del bot",
   //permission: "SEND_MESSAGES"
   run: async (interaction, client, args) => {
     await interaction.followUp({ content: `Mi Ping Es: ${client.ws.ping}` });
