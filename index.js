f2094a99904ce2c8df9fba8cda0ffd4544569b21
unknown_bom <73638811+AahhsSjsj@users.noreply.github.com>
2021-10-05 02:09:25 -0500

    Discord Bot V13

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const { Client, Collection } = require('discord.js')
-const client = new Client({ intents: 32767 }) // Para Cambiar A Otros Intents Entra A Esta Pagina Para Escoger Tus Intents: https://ziad87.net/intents/
-const { token } = require('./config.json')
+const { Client, Collection } = require("discord.js");
+const client = new Client({ intents: 32767 }); // Para Cambiar A Otros Intents Entra A Esta Pagina Para Escoger Tus Intents: https://ziad87.net/intents/
+const { token } = require("./config.json");
 
 client.Slashcommands = new Collection();
 client.commands = new Collection();
 client.cooldowns = new Collection();
 client.aliases = new Collection();
 
-require('./Handlers/Events')(client);
-require('./Handlers/SlashCommands')(client);
-require('./Handlers/Commands')(client);
+require("./Handlers/Events")(client);
+require("./Handlers/SlashCommands")(client);
+require("./Handlers/Commands")(client);
 
-client.login(token)
+client.login(token);
