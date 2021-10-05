f2d200cb1fdd7f42fccbba843eea695549982a09
unknown_bom <73638811+AahhsSjsj@users.noreply.github.com>
2021-10-05 01:52:48 -0500

    Discord Bot V13

--- Handlers/Events.js
+++ Handlers/Events.js
@@ -4,29 +4,31 @@ const { glob } = require("glob");
 const PG = promisify(glob);
 const Ascii = require("ascii-table");
 
+module.exports = async (client) => {
+  var Table = new Ascii("Eventos Cargados");
 
-module.exports = (client, Discord) => {
-    var Table = new Ascii("Eventos Cargados")
+  await (
+    await PG(`${process.cwd()}/Events/*/*.js`)
+  ).map(async (file) => {
+    const event = require(file);
 
-    await (await PG(`${process.cwd()}/Events/*/*.js`)).map(async (file) => {
-         const event = require(file);
- 
-         if(!Events.includes(event.name) || !event.name) {
-             const L = file.split("/")
-             await Table.addRow(`${event.name || "MISSING"}`, `📛 El nombre del evento no es válido o falta: ${L[5] + `/` + L[6]}`)
-             return;
-         }
- 
-         
-         if(event.once) {
-             client.once(event.name, (...args) => event.run(...args, client))
-         } else {
-             client.on(event.name, (...args) => event.run(...args, client))
-         };
- 
-         await Table.addRow(event.name, "✔ EXITOSO")
-     });
- 
-     console.log(Table.toString());
- 
+    if (!Events.includes(event.name) || !event.name) {
+      const L = file.split("/");
+      await Table.addRow(
+        `${event.name || "MISSING"}`,
+        `📛 El nombre del evento no es válido o falta: ${L[5] + `/` + L[6]}`
+      );
+      return;
+    }
+
+    if (event.once) {
+      client.once(event.name, (...args) => event.run(...args, client));
+    } else {
+      client.on(event.name, (...args) => event.run(...args, client));
+    }
+
+    await Table.addRow(event.name, "✔ EXITOSO");
+  });
+
+  console.log(Table.toString());
 };
